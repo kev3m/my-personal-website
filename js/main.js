430cb5cd21f2abc5980701dee3452043e25aebec
unknown <keven_jkc@hotmail.com>
2021-10-04 09:51:42 -0300

    website files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const revealerNav = window.revealer({
+        revealElementSelector: ".nav-js",
+        options: {
+            anchorSelector: ".nav-btn-js",
+        },
+    });
+
+    const actionBtn = document.querySelector(".nav-btn-js");
+    actionBtn.addEventListener("click", () => {
+        if (!revealerNav.isRevealed()) {
+            revealerNav.reveal();
+            actionBtn.setAttribute("data-open", true);
+        } else {
+            revealerNav.hide();
+            actionBtn.setAttribute("data-open", false);
+        }
+    });
+});
+
+$('.js-menu').on('click', function(e) {
+    e.preventDefault();
+    
+    $(this).toggleClass('active');
+  });
