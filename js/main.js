6a3761bd52dd6c333d87f76c0dda569bc4709092
unknown <keven_jkc@hotmail.com>
2021-10-21 13:59:15 -0300

    obg jonata te amo

--- js/main.js
+++ js/main.js
@@ -16,24 +16,34 @@ document.addEventListener("DOMContentLoaded", () => {
             actionBtn.setAttribute("data-open", false);
         }
     });
+    const linksDoMenuCaralho = document.getElementsByClassName("header__menu-item")
+    for (let elm of linksDoMenuCaralho) {
+        elm.onclick = () => {
+            revealerNav.hide();
+            document.getElementById("jonatasesteveaqui").setAttribute("data-open", false);
+        }
+    }
 });
 
-$('.nav-btn-js  ').on('click','a', function(e) {
+
+
+
+$('.nav-button').on('click', 'a', function (e) {
     e.preventDefault();
-    
+
     $(this).toggleClass('active');
-  });
+});
 
-  AOS.init();
+AOS.init();
 
-  let innerCursor = document.querySelector('.inner-cursor');
-  let outerCursor = document.querySelector('.outer-cursor');
+let innerCursor = document.querySelector('.inner-cursor');
+let outerCursor = document.querySelector('.outer-cursor');
 
-  document.addEventListener('mousemove', moveCursor);
+document.addEventListener('mousemove', moveCursor);
 
-  function moveCursor(e) {
-      let x = e.clientX;
-      let y = e.clientY;
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
 
     //   console.log(x, y)
 
@@ -41,15 +51,36 @@ $('.nav-btn-js  ').on('click','a', function(e) {
     innerCursor.style.top = `${y}px`
     outerCursor.style.left = `${x}px`
     outerCursor.style.top = `${y}px`
-  }
+}
 
-  let links = Array.from(document.querySelectorAll("a"));
+let links = Array.from(document.querySelectorAll("a"));
 
-  links.forEach(link => {
-      link.addEventListener('mouseover', ()=>{
-          innerCursor.classList.add("grow")
-      })
-      link.addEventListener('mouseleave', ()=>{
-          innerCursor.classList.remove("grow")
-      })
-  })
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        innerCursor.classList.add("grow")
+    })
+    link.addEventListener('mouseleave', () => {
+        innerCursor.classList.remove("grow")
+    })
+})
+
+const button = document.querySelector('.submit-button'),
+    stateMsg = document.querySelector('.pre-state-msg');
+
+const updateButtonMsg = function () {
+    button.classList.add('state-1', 'animated');
+
+    setTimeout(finalButtonMsg, 2000);
+};
+
+const finalButtonMsg = function () {
+    button.classList.add('state-2');
+
+    setTimeout(setInitialButtonState, 2000);
+};
+
+const setInitialButtonState = function () {
+    button.classList.remove('state-1', 'state-2', 'animated');
+};
+
+button.addEventListener('click', updateButtonMsg);
